b6b242ddd5a33dc0ad81d82cded0281134685406
Andrew Dodd <atd7@cornell.edu>
2012-12-10 08:26:54 -0500

    DisplayDevice: Backwards compatibility with old EGL
    From 4.1 to 4.2, the display subsystem was reworked to
    use SurfaceTextureClient/BufferQueue instead of
    FramebufferNativeWindow for the framebuffer itself.
    
    Unfortunately, some legacy EGL libraries make assumptions
    that any framebuffer device will be FramebufferNativeWindow.
    
    These EGL libraries will fail when used in 4.2 as if the
    framebuffer is not FramebufferNativeWindow, they will
    try to dequeue more than one buffer at a time, which
    will cause a hang of the graphics subsystem.
    
    This allows use of FramebufferNativeWindow to keep
    legacy EGL implementations happy.  Confirmed EGL
    implementations that need this include but are
    not limited to:
    
    Set BOARD_EGL_NEEDS_LEGACY_FB to use
    
    Tegra2/3 ICS EGL
    Mali400 ICS EGL (Allwinner A10)
    Mali400 Jellybean (4.1) EGL (Samsung Exynos4)
    
    Original implementation by OndraOrg of XDA:
    http://forum.xda-developers.com/showpost.php?p=34469675&postcount=19
    
    Change-Id: I74005cf6753b3c9bfb4c1e32fb641f5167787917
    
    Conflicts:
    	services/surfaceflinger/Android.mk
    	services/surfaceflinger/DisplayDevice.cpp
    
    Change-Id: Ib8455b33bc65959d0ccc2333fc480d506e57e740

--- services/surfaceflinger/DisplayDevice.cpp
+++ services/surfaceflinger/DisplayDevice.cpp
@@ -29,6 +29,14 @@
 
 #include <gui/Surface.h>
 
+#ifdef BOARD_EGL_NEEDS_LEGACY_FB
+#include <ui/FramebufferNativeWindow.h>
+#endif
+
+#include <GLES/gl.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+
 #include <hardware/gralloc.h>
 
 #include "DisplayHardware/DisplaySurface.h"
@@ -77,7 +85,11 @@ DisplayDevice::DisplayDevice(
       mActiveConfig(0)
 {
     mNativeWindow = new Surface(producer, false);
+#ifndef BOARD_EGL_NEEDS_LEGACY_FB
     ANativeWindow* const window = mNativeWindow.get();
+#else
+    ANativeWindow* const window = new FramebufferNativeWindow();
+#endif
 
     /*
      * Create our display's surface
