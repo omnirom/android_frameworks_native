efbe327f220b3e3246a24e63cf0ba3bcd0db9194
Sushil Chauhan <sushilchauhan@codeaurora.org>
2013-03-22 13:38:13 -0700

    Init display orientation according to panel orientation
       - Initialize the display default orientation to panel orientation.
       - honor the panel orientation only after the end of boot animation
         or the new orientation is as same as panel orientation.
    
    Change-Id: Ieece097ff7c3710e379f47028900aed59a96c8d5

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -634,6 +634,7 @@ int32_t SurfaceFlinger::allocateHwcDisplayId(DisplayDevice::DisplayType type) {
 
 void SurfaceFlinger::startBootAnim() {
     // start boot animation
+    mBootFinished = false;
     property_set("service.bootanim.exit", "0");
     property_set("ctl.start", "bootanim");
 }
@@ -1352,8 +1353,14 @@ void SurfaceFlinger::handleTransactionLocked(uint32_t transactionFlags)
                                 || (state.viewport != draw[i].viewport)
                                 || (state.frame != draw[i].frame))
                         {
-                            disp->setProjection(state.orientation,
-                                    state.viewport, state.frame);
+                            // Honor the orientation change after boot
+                            // animation completes or the new orientation is
+                            // same as panel orientation..
+                            if(mBootFinished ||
+                               state.orientation == disp->getOrientation()) {
+                                disp->setProjection(state.orientation,
+                                        state.viewport, state.frame);
+                            }
                         }
                     }
                 }
