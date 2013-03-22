92bdee73e866147220c6be2e02c1ae9dd3d70308
Sushil Chauhan <sushilchauhan@codeaurora.org>
2013-03-22 13:38:13 -0700

    native: [1/2]: squashed commits for hwrotation
    [2/2]: https://gerrit.omnirom.org/#/c/10965/
    
    -------------------------
    native: init display orientation according to panel orientation
    
       - Initialize the display default orientation to panel orientation.
       - honor the panel orientation only after the end of boot animation
         or the new orientation is as same as panel orientation.
    
    Change-Id: I37c24cb89fb6a31910b6d28c303925bac3d9c976
    -------------------------
    native: surfaceflinger: reimplement hwrotation
    
    Change-Id: I1bcabed36bfeaed087e3b9842a46454a20a919ec
    -------------------------
    native: surfaceflinger: odd hw rotation (90/270) patch for swapping width/height
    
    This patch works in addition to the following commit
    
    re-implementing ro.sf.hwrotation:
    https://github.com/CyanogenMod/android_frameworks_native/commit/7d283431efffc4402cb1a6cacf5da64729c883bb
    
    When using values of 90 and 270 for ro.sf.hwrotation the
    LCD width and height also need to be swapped to display properly.
    
    NOTE: This patch does not fix the initial startup of bootanimation
    
    Change-Id: If5415aa08eef4749672baf42993af5c9ac7e5401
    -------------------------
    native: surfaceflinger: fix boot animation rotation problem
    
    when ro.sf.hwrotation is set to 90 or 270
    
    Change-Id: I836db729cc4e953f092538ea62772202517f70f9
    -------------------------
    native: surfaceflinger: consolidate display orientation compensation hooks
    
    Dedupe rotation calculation code and make rotation a statically
    available property in the DisplayDevice
    
    Change-Id: I7b38af14d9bb8d6b0df511c70da043914956415f

--- services/surfaceflinger/DisplayDevice.cpp
+++ services/surfaceflinger/DisplayDevice.cpp
@@ -72,6 +72,7 @@ DisplayDevice::DisplayDevice(
       mIsSecure(isSecure),
       mSecureLayerVisible(false),
       mLayerStack(NO_LAYER_STACK),
+      mHardwareOrientation(0),
       mOrientation(),
       mPowerMode(HWC_POWER_MODE_OFF),
       mActiveConfig(0)
@@ -119,7 +120,12 @@ DisplayDevice::DisplayDevice(
     // was created with createDisplay().
     switch (mType) {
         case DISPLAY_PRIMARY:
+            char value[PROPERTY_VALUE_MAX];
             mDisplayName = "Built-in Screen";
+
+            /* hwrotation applies only to the primary display */
+            property_get("ro.sf.hwrotation", value, "0");
+            mHardwareOrientation = atoi(value);
             break;
         case DISPLAY_EXTERNAL:
             mDisplayName = "HDMI Screen";
@@ -129,8 +135,14 @@ DisplayDevice::DisplayDevice(
             break;
     }
 
+    char property[PROPERTY_VALUE_MAX];
+    int panelOrientation = DisplayState::eOrientationDefault;
+    // Set the panel orientation from the property.
+    property_get("persist.panel.orientation", property, "0");
+    panelOrientation = atoi(property) / 90;
+
     // initialize the display orientation transform.
-    setProjection(DisplayState::eOrientationDefault, mViewport, mFrame);
+    setProjection(panelOrientation, mViewport, mFrame);
 }
 
 DisplayDevice::~DisplayDevice() {
@@ -380,6 +392,18 @@ status_t DisplayDevice::orientationToTransfrom(
         int orientation, int w, int h, Transform* tr)
 {
     uint32_t flags = 0;
+    int additionalRot = this->getHardwareOrientation();
+
+    if (additionalRot) {
+        additionalRot /= 90;
+        if (orientation == DisplayState::eOrientationUnchanged) {
+            orientation = additionalRot;
+        } else {
+            orientation += additionalRot;
+            orientation %= 4;
+        }
+    }
+
     switch (orientation) {
     case DisplayState::eOrientationDefault:
         flags = Transform::ROT_0;
@@ -396,6 +420,7 @@ status_t DisplayDevice::orientationToTransfrom(
     default:
         return BAD_VALUE;
     }
+
     tr->set(flags, w, h);
     return NO_ERROR;
 }
@@ -435,7 +460,11 @@ void DisplayDevice::setProjection(int orientation,
     if (!frame.isValid()) {
         // the destination frame can be invalid if it has never been set,
         // in that case we assume the whole display frame.
-        frame = Rect(w, h);
+        if ((mHardwareOrientation/90) & DisplayState::eOrientationSwapMask) {
+            frame = Rect(h, w);
+        } else {
+            frame = Rect(w, h);
+        }
     }
 
     if (viewport.isEmpty()) {
@@ -488,6 +517,10 @@ void DisplayDevice::setProjection(int orientation,
     mOrientation = orientation;
     mViewport = viewport;
     mFrame = frame;
+}
+
+int DisplayDevice::getHardwareOrientation() {
+    return mHardwareOrientation;
 }
 
 void DisplayDevice::dump(String8& result) const {
