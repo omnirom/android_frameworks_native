f6741b3861a4d264342970ded009e5e0386a3e7b
xplodwild <me@xplod.fr>
2013-11-05 08:09:28 +0100

    Fix mismerge
    Change-Id: I8ebfbd9736bd43c19c409c96015a02c7c2683026

--- libs/ui/GraphicBufferAllocator.cpp
+++ libs/ui/GraphicBufferAllocator.cpp
@@ -110,7 +110,7 @@ status_t GraphicBufferAllocator::alloc(uint32_t w, uint32_t h, PixelFormat forma
     }
     ALOGW_IF(err, "alloc(%u, %u, %d, %08x, %d ...) failed %d (%s)",
             w, h, format, usage, bufferSize, err, strerror(-err));
-#else
+#endif
 
 #ifdef EXYNOS4_ENHANCEMENTS
     if ((format == 0x101) || (format == 0x105) || (format == 0x107)) {
@@ -121,7 +121,9 @@ status_t GraphicBufferAllocator::alloc(uint32_t w, uint32_t h, PixelFormat forma
     }
 #endif
 
+#ifndef QCOM_BSP
     err = mAllocDev->alloc(mAllocDev, w, h, format, usage, handle, stride);
+#endif
 
     ALOGW_IF(err, "alloc(%u, %u, %d, %08x, ...) failed %d (%s)",
             w, h, format, usage, err, strerror(-err));
