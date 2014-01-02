7f2430c78b36b7e87cc7d1582c15028e30491c9e
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-01-02 17:38:39 +0100

    frameworks/native: Add support for Tegra3 based Devices by adding options * BOARD_EGL_SKIP_FIRST_DEQUEUE to add back the legacy EGL workaround * BOARD_USE_MHEAP_SCREENSHOT to support mHeap-based Screenshots Those Changes are taken from * CM (see https://github.com/CyanogenMod/android_frameworks_native/commit/c3b18f73fd7ff58794f39ad36a49c7bce996967c) * CM (see http://review.cyanogenmod.org/#/c/53162/)
    Change-Id: I41be2cedd54ed6f72d1a498b1d11e8768406fe0e

--- libs/gui/Android.mk
+++ libs/gui/Android.mk
@@ -39,6 +39,7 @@ LOCAL_SHARED_LIBRARIES := \
 	libutils \
 	liblog
 
+
 ifeq ($(call is-board-platform-in-list, mpq8092), true)
     LOCAL_CFLAGS            += -DVFM_AVAILABLE
 endif
@@ -55,6 +56,14 @@ endif
     LOCAL_C_INCLUDES        += $(TARGET_OUT_HEADERS)/vpu/
     LOCAL_CFLAGS            += -DQCOM_BSP
     LOCAL_SHARED_LIBRARIES  += libqdMetaData
+endif
+
+ifeq ($(BOARD_EGL_SKIP_FIRST_DEQUEUE),true)
+    LOCAL_CFLAGS += -DSURFACE_SKIP_FIRST_DEQUEUE
+endif
+
+ifeq ($(BOARD_USE_MHEAP_SCREENSHOT),true)
+    LOCAL_CFLAGS += -DUSE_MHEAP_SCREENSHOT
 endif
 
 LOCAL_MODULE:= libgui
