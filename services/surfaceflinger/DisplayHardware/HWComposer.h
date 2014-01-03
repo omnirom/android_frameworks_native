9a343f1e4add0ab76aaf6393cfcb4031994f72dc
Schischu <schischu65@gmail.com>
2014-01-03 12:59:02 -0500

    Enable the use of old HWComposer APi
    Tegra 2 devices like TF101 need this to stop surfaceflinger from crashing, causing hotboots
    Board flag is BOARD_NEEDS_OLD_HWC_API := true
    
    Change-Id: I931be737b809427c6ce1c9f61200687450b14a8d

--- services/surfaceflinger/DisplayHardware/HWComposer.h
+++ services/surfaceflinger/DisplayHardware/HWComposer.h
@@ -159,7 +159,9 @@ public:
         virtual sp<Fence> getAndResetReleaseFence() = 0;
         virtual void setDefaultState() = 0;
         virtual void setSkip(bool skip) = 0;
+#ifndef OLD_HWC_API
         virtual void setAnimating(bool animating) = 0;
+#endif
         virtual void setBlending(uint32_t blending) = 0;
         virtual void setTransform(uint32_t transform) = 0;
         virtual void setFrame(const Rect& frame) = 0;
