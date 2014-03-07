2cbd1200b889391dc457aa9f22358cdc6374a886
Tesla-MADAL93 <mattia.dalleva1993@gmail.com>
2014-03-07 16:29:31 +0100

    [2/3] STE OMX Fix
    Fix Camera, Camcorder and Torch on STE Devices
    
     PS2: removed whitespaces and tabspaces
     PS5: Conflicts: include/binder/Parcel.h
    
    Change-Id: Ibb6be5289670774a541381bbffc514744e20fdcb

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -819,10 +819,16 @@ status_t Parcel::writeBlob(size_t len, WritableBlob* outBlob)
     return status;
 }
 
+#ifdef STE_HARDWARE
+status_t Parcel::write(const Flattenable& val) {
+    const FlattenableHelper helper(val);
+    return write(helper);
+#else
 extern "C" status_t _ZN7android6Parcel5writeERKNS0_26FlattenableHelperInterfaceE(void *parcel, void *val);
 
 extern "C" status_t _ZN7android6Parcel5writeERKNS_11FlattenableE(void *parcel, void *val) {
     return _ZN7android6Parcel5writeERKNS0_26FlattenableHelperInterfaceE(parcel, val);
+#endif
 }
 
 status_t Parcel::write(const FlattenableHelperInterface& val)
@@ -1203,10 +1209,16 @@ status_t Parcel::readBlob(size_t len, ReadableBlob* outBlob) const
     return NO_ERROR;
 }
 
+#ifdef STE_HARDWARE
+status_t Parcel::read(Flattenable& val) const {
+    FlattenableHelper helper(val);
+    return read(helper);
+#else
 extern "C" status_t _ZNK7android6Parcel4readERNS0_26FlattenableHelperInterfaceE(void *parcel, void *val);
 
 extern "C" status_t _ZNK7android6Parcel4readERNS_11FlattenableE(void *parcel, void *val) {
     return _ZNK7android6Parcel4readERNS0_26FlattenableHelperInterfaceE(parcel, val);
+#endif
 }
 
 status_t Parcel::read(FlattenableHelperInterface& val) const
