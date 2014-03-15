1b31653624efe989352807bf1ad4f344c5d66bbd
olivieer <y.karasangabo@bg-rams.ac.at>
2014-03-15 11:29:02 -0400

    frameworks/native: hw overlay bootlogo fix
    add hack to surfaceflinger initialization to stop bootlogo showing
    incorrectly on app preview background and other places on the Sony
    Xperia S
    Original commit:
    https://github.com/dmcoles/android_frameworks_native/commit/f21f0062373b69a5c565442204fbbf160d06d030
    
    Change-Id: I986cd1fec94ebfb620082b6a94188b9c7a91fb1c

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2380,6 +2380,8 @@ void SurfaceFlinger::onInitializeDisplays() {
     Vector<ComposerState> state;
     Vector<DisplayState> displays;
     DisplayState d;
+    sp<const DisplayDevice> hw(getDefaultDisplayDevice());
+
     d.what = DisplayState::eDisplayProjectionChanged |
              DisplayState::eLayerStackChanged;
     d.token = mBuiltinDisplays[DisplayDevice::DISPLAY_PRIMARY];
@@ -2389,6 +2391,8 @@ void SurfaceFlinger::onInitializeDisplays() {
     d.viewport.makeInvalid();
     displays.add(d);
     setTransactionState(state, displays, 0);
+    
+    getHwComposer().release(hw->getDisplayType());
     onScreenAcquired(getDefaultDisplayDevice());
 
     const nsecs_t period =
