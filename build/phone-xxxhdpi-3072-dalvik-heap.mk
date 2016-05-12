78f16624953bbd80a6478da32ffa455d28e87553
maxwen <max.weninger@gmail.com>
2016-05-12 23:34:53 +0200

    native: sync dalvik config dalvik.vm.heapstartsize
    no idea why the 2048 has 16m but 3072 only 8?
    
    Change-Id: I99bd6703f85657eaa364236a0b0c5552f7e42b27

--- build/phone-xxxhdpi-3072-dalvik-heap.mk
+++ build/phone-xxxhdpi-3072-dalvik-heap.mk
@@ -17,7 +17,7 @@
 # Provides overrides to configure the Dalvik heap for a 3G phone
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
