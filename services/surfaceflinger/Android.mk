5cd1f873a7fdc030e0154f4543640c042f4383cf
Jake Whatley <jacob.whatley91@gmail.com>
2016-05-31 04:53:15 +0200

    [TESTING] native-caf: merge of LA.BF64.1.2.2-04340-8x92.0 [DO NOT MERGE]
    Change-Id: I3754e1d05a3885028399112cf654289e31b2e25c

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -35,9 +35,18 @@ LOCAL_SRC_FILES := \
     RenderEngine/GLES10RenderEngine.cpp \
     RenderEngine/GLES11RenderEngine.cpp \
     RenderEngine/GLES20RenderEngine.cpp \
-    DisplayUtils.cpp
+    DisplayUtils.cpp \
+    ExSurfaceFlinger/ExLayer.cpp \
+    ExSurfaceFlinger/ExSurfaceFlinger.cpp \
+    ExSurfaceFlinger/ExVirtualDisplaySurface.cpp \
+    ExSurfaceFlinger/ExHWComposer.cpp
 
 LOCAL_CFLAGS := -DLOG_TAG=\"SurfaceFlinger\"
+
+ifeq ($(TARGET_BUILD_VARIANT),userdebug)
+LOCAL_CFLAGS += -DDEBUG_CONT_DUMPSYS
+endif
+
 LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES -DEGL_EGLEXT_PROTOTYPES
 
 ifeq ($(TARGET_BOARD_PLATFORM),omap4)
@@ -105,10 +114,6 @@ LOCAL_SHARED_LIBRARIES := \
     libui \
     libgui \
     libpowermanager
-
-LOCAL_WHOLE_STATIC_LIBRARIES += libexsurfaceflinger
-
-LOCAL_C_INCLUDES += $(ANDROID_BUILD_TOP)/vendor/qcom/opensource/display-frameworks/native/services/surfaceflinger/
 
 ifeq ($(TARGET_USES_QCOM_BSP), true)
 ifneq ($(TARGET_QCOM_DISPLAY_VARIANT),)
