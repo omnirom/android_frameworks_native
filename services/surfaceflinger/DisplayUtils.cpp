8f179fa5aea67baa080b5efe8b249855b8a93a68
Jake Whatley <jacob.whatley91@gmail.com>
2016-05-31 04:53:15 +0200

    [TESTING] native-caf: merge of LA.BF64.1.2.2-04340-8x92.0 [DO NOT MERGE]
    Change-Id: I3754e1d05a3885028399112cf654289e31b2e25c

--- services/surfaceflinger/DisplayUtils.cpp
+++ services/surfaceflinger/DisplayUtils.cpp
@@ -39,11 +39,13 @@
 #include "RenderEngine/RenderEngine.h"
 #include "DisplayHardware/FramebufferSurface.h"
 #include "DisplayUtils.h"
-#include <ExSurfaceFlinger.h>
-#include <ExLayer.h>
-#include <DisplayHardware/ExHWComposer.h>
-#include <DisplayHardware/ExVirtualDisplaySurface.h>
-#include <dlfcn.h>
+#include <ExSurfaceFlinger/ExSurfaceFlinger.h>
+#include <ExSurfaceFlinger/ExLayer.h>
+#include <ExSurfaceFlinger/ExHWComposer.h>
+#include <ExSurfaceFlinger/ExVirtualDisplaySurface.h>
+#if QTI_BSP
+#include <gralloc_priv.h>
+#endif
 
 namespace android {
 
@@ -155,6 +157,18 @@ bool DisplayUtils::createV4L2BasedVirtualDisplay(HWComposer* hwc, int32_t &hwcDi
         return true;
     }
     return false;
+}
+
+bool DisplayUtils::canAllocateHwcDisplayIdForVDS(int usage) {
+    // on AOSP builds with QTI_BSP disabled, we should allocate hwc display id for virtual display
+    int flag_mask = 0xffffffff;
+
+#if QTI_BSP
+    // Reserve hardware acceleration for WFD use-case
+    flag_mask = GRALLOC_USAGE_PRIVATE_WFD;
+#endif
+
+    return (usage & flag_mask);
 }
 
 }; // namespace android
