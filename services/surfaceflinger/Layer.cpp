eb2e4ee2519f67b15c5de012581b2d7d702a087b
Pullakavi Srinivas <spullaka@codeaurora.org>
2017-02-16 16:21:47 +0530

    sf: Fix GPU coordinates computation.
    Along with Viewport, final crop need to be
    considered to maintain aspect ratio.
    
    CRs-Fixed: 2001126
    Change-Id: Ia499cc059a2b0ef65002c60215d2f01ac706867f

--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -1065,6 +1065,11 @@ void Layer::drawWithOpenGL(const sp<const DisplayDevice>& hw,
 
         win = s.active.transform.transform(win);
         win.intersect(hw->getViewport(), &win);
+        if (!s.finalCrop.isEmpty()) {
+            if (!win.intersect(s.finalCrop, &win)) {
+                 win.clear();
+            }
+        }
         win = s.active.transform.inverse().transform(win);
         win.intersect(Rect(s.active.w, s.active.h), &win);
         win = reduce(win, s.activeTransparentRegion);
@@ -1298,6 +1303,11 @@ void Layer::computeGeometry(const sp<const DisplayDevice>& hw, Mesh& mesh,
     if((hw_w * hw_h) > NUM_PIXEL_LOW_RES_PANEL) {
         win = s.active.transform.transform(win);
         win.intersect(hw->getViewport(), &win);
+        if (!s.finalCrop.isEmpty()) {
+            if (!win.intersect(s.finalCrop, &win)) {
+                 win.clear();
+            }
+        }
         win = s.active.transform.inverse().transform(win);
         win.intersect(Rect(s.active.w, s.active.h), &win);
         win = reduce(win, s.activeTransparentRegion);
