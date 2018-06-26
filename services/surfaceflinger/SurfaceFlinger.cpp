e47ee929b3d4033c059b743e4703e2372a589693
Chia-I Wu <olv@google.com>
2018-06-26 10:18:18 +0800

    surfaceflinger: signalRefresh after boot animation starts
    Assume BootStage::BOOTLOADER initially.  Switch to
    BootStage::BOOTANIMATION on first buffer latch and switch to
    BootStage::FINISHED after bootFinished is called.
    
    Do not invoke signalRefresh when in BootStage::BOOTLOADER.  We do
    not want to replace bootloader splash by a blank screen.  This saves
    HWC from workarounds that may or may not work reliably.
    
    Bug: 79434305
    Bug: 110772452
    Test: reboot and observe
    CRs-Fixed: 2336841
    Change-Id: I9e892e629303177431acd2cfe23f0f984ca6866e
    Merged-In: I9e892e629303177431acd2cfe23f0f984ca6866e
    (cherry picked from commit ad8d827e921250c9b5cc52922bc76852136cb49c)

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -251,6 +251,7 @@ SurfaceFlinger::SurfaceFlinger(SurfaceFlinger::SkipInitializationTag)
         mVisibleRegionsDirty(false),
         mGeometryInvalid(false),
         mAnimCompositionPending(false),
+        mBootStage(BootStage::BOOTLOADER),
         mActiveDisplays(0),
         mBuiltInBitmask(0),
         mDebugRegion(0),
@@ -261,7 +262,6 @@ SurfaceFlinger::SurfaceFlinger(SurfaceFlinger::SkipInitializationTag)
         mLastSwapBufferTime(0),
         mDebugInTransaction(0),
         mLastTransactionTime(0),
-        mBootFinished(false),
         mForceFullDamage(false),
         mPrimaryDispSync("PrimaryDispSync"),
         mPrimaryHWVsyncEnabled(false),
@@ -513,6 +513,7 @@ void SurfaceFlinger::bootFinished()
 
     sp<LambdaMessage> readProperties = new LambdaMessage([&]() {
         readPersistentProperties();
+        mBootStage = BootStage::FINISHED;
     });
     postMessageAsync(readProperties);
 }
@@ -1533,7 +1534,7 @@ void SurfaceFlinger::onMessageReceived(int32_t what) {
             bool refreshNeeded = handleMessageTransaction();
             refreshNeeded |= handleMessageInvalidate();
             refreshNeeded |= mRepaintEverything;
-            if (refreshNeeded) {
+            if (refreshNeeded && CC_LIKELY(mBootStage != BootStage::BOOTLOADER)) {
                 // Signal a refresh if a transaction modified the window state,
                 // a new buffer was latched, or if HWC has requested a full
                 // repaint
@@ -2936,6 +2937,12 @@ bool SurfaceFlinger::handlePageFlip()
     // up during the next vsync period to check again.
     if (frameQueued && (mLayersWithQueuedFrames.empty() || !newDataLatched)) {
         signalLayerUpdate();
+    }
+
+    // enter boot animation on first buffer latch
+    if (CC_UNLIKELY(mBootStage == BootStage::BOOTLOADER && newDataLatched)) {
+        ALOGI("Enter boot animation");
+        mBootStage = BootStage::BOOTANIMATION;
     }
 
     // Only continue with the refresh if there is actually new work to do
