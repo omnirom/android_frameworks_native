ab6a5a11622e853cfe7633cb009f1df82360ce8e
Rajavenu Kyatham <rkyatham@codeaurora.org>
2019-02-05 19:25:06 +0530

    SurfaceFlinger: Fix virtual display related issues.
    1) Initialize framebuffer slots for virtual display
    
    2) Validate output buffer usage bit appropriately
    
    Validate output buffer usage bit only against GRALLOC_USAGE_HW_COMPOSER
    to differentiate GLES only conposition and HWC/MIXED composition.
    
    3) Exclude video encoder usage for scratch buffer
    
    Sink and Scratch buffers in VDS are using same usage flags.
    This causes video encoder flag to be set for scratch
    buffers Also. Exclude video encoder flag from scratch
    buffer usage flags as scratch buffers are used only as
    write back input and not video encoder input.
    
    Bug: 126495801
    
    Change-Id: Ia49a175372ca187a295531e18f8e84dc22a19486
    CRs-Fixed: 1008906

--- services/surfaceflinger/DisplayHardware/VirtualDisplaySurface.cpp
+++ services/surfaceflinger/DisplayHardware/VirtualDisplaySurface.cpp
@@ -163,7 +163,7 @@ status_t VirtualDisplaySurface::prepareFrame(CompositionType compositionType) {
 
     if (mCompositionType != COMPOSITION_GLES &&
             (mOutputFormat != mDefaultOutputFormat ||
-             mOutputUsage != GRALLOC_USAGE_HW_COMPOSER)) {
+             !(mOutputUsage & GRALLOC_USAGE_HW_COMPOSER))) {
         // We must have just switched from GLES-only to MIXED or HWC
         // composition. Stop using the format and usage requested by the GLES
         // driver; they may be suboptimal when HWC is writing to the output
@@ -323,6 +323,13 @@ status_t VirtualDisplaySurface::setAsyncMode(bool async) {
 status_t VirtualDisplaySurface::dequeueBuffer(Source source,
         PixelFormat format, uint64_t usage, int* sslot, sp<Fence>* fence) {
     LOG_FATAL_IF(!mDisplayId);
+
+    // Exclude video encoder usage flag from scratch buffer usage flags.
+    if (source == SOURCE_SCRATCH) {
+        usage &= ~(GRALLOC_USAGE_HW_VIDEO_ENCODER);
+        VDS_LOGV("dequeueBuffer(%s): updated scratch buffer usage flags=%#" PRIx64,
+                dbgSourceStr(source), usage);
+    }
 
     status_t result =
             mSource[source]->dequeueBuffer(sslot, fence, mSinkBufferWidth, mSinkBufferHeight,
