4837ee0702f689695a350a94601e5368a6372bf6
Alex Sarraf <asarraf@codeaurora.org>
2019-08-01 09:51:53 -0700

    sf: updateVsyncSource only for internal displays
    Update the vsync source only when there is are internal displays.
    Prevents call from external displays setting power mode.
    
    Change-Id: I6ffe6d74191035e2b11197bed662d088afebbddc
    CRs-Fixed: 2416529

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -4817,7 +4817,7 @@ void SurfaceFlinger::setPowerModeInternal(const sp<DisplayDevice>& display, int 
                 mScheduler->onScreenAcquired(mAppConnectionHandle);
                 mScheduler->resyncToHardwareVsync(true, getVsyncPeriod());
             }
-        } else {
+        } else if (displayId == getInternalDisplayIdLocked()) {
             updateVsyncSource();
         }
 
