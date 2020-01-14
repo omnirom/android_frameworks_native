8c8bcadee6e0292f760c9c09a54c50bf14bd2599
Rajavenu Kyatham <rkyatham@codeaurora.org>
2020-01-14 18:51:14 +0530

    SurfaceFlinger: Allow IdleFallback on start
    - By default, IdleFallback will be disabled.
      On successful compser client creation, set
      idle time.
    
    Change-Id: I33f3a52f857bc360522327e4d181a84f5310bdd8
    CRs-Fixed: 2694287

--- services/surfaceflinger/DisplayHardware/ComposerHal.cpp
+++ services/surfaceflinger/DisplayHardware/ComposerHal.cpp
@@ -27,6 +27,7 @@
 #include <hidl/HidlTransportSupport.h>
 #include <hidl/HidlTransportUtils.h>
 #include <vendor/qti/hardware/display/composer/2.1/IQtiComposerClient.h>
+#include <vendor/display/config/1.16/IDisplayConfig.h>
 
 namespace android {
 
@@ -218,6 +219,17 @@ Composer::Composer(const std::string& serviceName)
 
     if (mClient == nullptr) {
         LOG_ALWAYS_FATAL("failed to create composer client");
+    }
+
+    // On successful creation of composer client only allowIdleFallback
+    if (mClient) {
+        using vendor::display::config::V1_16::IDisplayConfig;
+        android::sp<IDisplayConfig> disp_config_v1_16 = IDisplayConfig::getService();
+        if (disp_config_v1_16 != NULL) {
+            if (disp_config_v1_16->allowIdleFallback()) {
+                ALOGW("failed to set Idle time");
+            }
+        }
     }
 
     if (mIsUsingVrComposer) {
