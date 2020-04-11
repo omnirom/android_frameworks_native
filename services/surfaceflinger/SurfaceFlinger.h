fb81c1a8e775eb10d697ed36aac4c0d713840cab
Ramakant Singh <ramakant@codeaurora.org>
2020-04-11 16:02:43 +0530

    sf:  Fix virtual display related issues.
    1. SurfaceFlinger: Fix virtual display related issues. 1) Validate
    output
    buffer usage bit appropriately Validate output buffer usage bit only
    against GRALLOC_USAGE_HW_COMPOSER to differentiate GLES only
    conposition
    and HWC/MIXED composition. 2) Exclude video encoder usage for
    scratch
    buffer Sink and Scratch buffers in VDS are using same usage
    flags. This
    causes video encoder flag to be set for scratch buffers
    Also. Exclude
    video encoder flag from scratch buffer usage flags as
    scratch buffers
    are used only as write back input and not video
    encoder input.
    
    2. sf: Allow VDS to use HWC -- Preserve VDS layer
    pixel format based on
    GRALLOC flags. -- skip color layer for vds
    
    3. sf: Add secure content support for VDS
    
    4. sf: Enable GPU comp. for non-primary displays
    Ensure that "Return
    status" of dequeueBuffer() complies with API
    	requirements. Client relies
    on this status to call requestBuffer()
    	upon reallocation.
    
    5. sf: Enable UBWC on virtual display scratch buffer Set
    	GRALLOC_USAGE_HW_FB usage flag on virtual display Scratch
    	buffer to
    	enable UBWC.
    
    Change-Id: Ia49a175372ca187a295531e18f8e84dc22a19486
    CRs-Fixed: 2656027

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -271,6 +271,8 @@ public:
 
     static bool useContextPriority;
 
+    static bool sDirectStreaming;
+
     // The data space and pixel format that SurfaceFlinger expects hardware composer
     // to composite efficiently. Meaning under most scenarios, hardware composer
     // will accept layers with the data space and pixel format.
@@ -748,6 +750,9 @@ private:
     void traverseLayersInDisplay(const sp<const DisplayDevice>& display,
                                  const LayerVector::Visitor& visitor);
 
+
+    bool canAllocateHwcDisplayIdForVDS(uint64_t usage);
+    bool skipColorLayer(const char* layerType);
     sp<StartPropertySetThread> mStartPropertySetThread;
 
     /* ------------------------------------------------------------------------
