59da727a82c5fe8bfd162c05f929c3769b1eddcc
Ray Zhang <rayz@codeaurora.org>
2020-07-03 10:26:12 +0800

    SurfaceFlinger: Use cached expected present time in one frame
    Make sure all cases use the same expected present time in one
    frame.
    
    CRs-Fixed: 2724607
    Change-Id: I8593167e27fdba72c357da0abb2a35b053967ffa

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2112,8 +2112,7 @@ void SurfaceFlinger::onMessageReceived(int32_t what) NO_THREAD_SAFETY_ANALYSIS {
                 int maxQueuedFrames = 0;
                 mDrawingState.traverseInZOrder([&](Layer* layer) {
                     if (layer->hasReadyFrame()) {
-                        nsecs_t expectedPresentTime;
-                        expectedPresentTime = mScheduler->getDispSyncExpectedPresentTime();
+                        nsecs_t expectedPresentTime = getExpectedPresentTime();
                         if (layer->shouldPresentNow(expectedPresentTime)) {
                             int layerQueuedFrames = layer->getQueuedFrameCount();
                             Mutex::Autolock lock(mDolphinStateLock);
