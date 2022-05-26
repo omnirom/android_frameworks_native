d5a37beee0573ffab71f6ed312713cf0f661ccc8
Rheygine Medel <quic_rmedel@quicinc.com>
2022-05-25 19:20:55 -0700

    sf: Clean up properties for virtual display
    Currently, the properties are used as follows:
    1. debug.sf.enable_hwc_vds - allows IDs to be generated for VDs
    2. vendor.display.vds_allow_hwc - allows WFD to use HWC path
    
    With this change, HWC path is enabled for WFD with only the vendor
    property set. All other virtual displays require both of the
    properties enabled.
    
    Change-Id: Iab2c8d15d2c1cf24be0d371af8892c346634507f
    CRs-Fixed: 3204941

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -1627,6 +1627,9 @@ private:
 
     const sp<WindowInfosListenerInvoker> mWindowInfosListenerInvoker;
 
+    bool mAllowHwcForVDS = false;
+    bool mAllowHwcForWFD = false;
+
     // returns the framerate of the layer with the given sequence ID
     float getLayerFramerate(nsecs_t now, int32_t id) const {
         return mScheduler->getLayerFramerate(now, id);
