41c22e49c0c4aaf3ce6403305f553b364825e726
LuK1337 <priv.luk@gmail.com>
2025-03-16 13:25:52 +0100

    fixup! surfaceflinger: Add support for Udfps extension lib
    Change-Id: I511d645e8fdd7e543df608b097c12e5a32032575
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- services/surfaceflinger/CompositionEngine/src/Output.cpp
+++ services/surfaceflinger/CompositionEngine/src/Output.cpp
@@ -1002,8 +1002,7 @@ compositionengine::OutputLayer* Output::findLayerRequestingBackgroundComposition
         // because that somehow leads to the Udfps touched layer getting device composition
         // consistently.
         if ((nextLayer != nullptr && layerRequestingBgComposition == nullptr) &&
-            (strncmp(nextLayer->getLayerFE().getDebugName(), UDFPS_TOUCHED_LAYER_NAME,
-                     strlen(UDFPS_TOUCHED_LAYER_NAME)) == 0)) {
+            (strstr(nextLayer->getLayerFE().getDebugName(), UDFPS_TOUCHED_LAYER_NAME) != nullptr)) {
             layerRequestingBgComposition = layer;
             break;
         }
