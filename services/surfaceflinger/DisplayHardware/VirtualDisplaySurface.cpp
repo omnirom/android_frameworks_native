81180be558a117de3feabfcc424fc3f73e443016
micky387 <mickaelsaibi@free.fr>
2025-12-19 22:11:25 -0500

    Revert "sf: Align virtual display changes with CAF"
    This reverts commit dc203cd71abf3ebe77c6564aee54b34be9a06460.
    
    Change-Id: I2cf8ff0f22f2c0147e270bac4e59d1be9e770880

--- services/surfaceflinger/DisplayHardware/VirtualDisplaySurface.cpp
+++ services/surfaceflinger/DisplayHardware/VirtualDisplaySurface.cpp
@@ -335,8 +335,8 @@ status_t VirtualDisplaySurface::dequeueBuffer(Source source,
     if (source == SOURCE_SCRATCH) {
         usage |= GRALLOC_USAGE_HW_FB;
         usage &= ~(GRALLOC_USAGE_HW_VIDEO_ENCODER);
-        VDS_LOGV("%s(%s): updated scratch buffer usage flags=%#" PRIx64,
-                __func__, ftl::enum_string(source).c_str(), usage);
+        VDS_LOGV("dequeueBuffer(%s): updated scratch buffer usage flags=%#" PRIx64,
+                dbgSourceStr(source), usage);
     }
 
     status_t result =
